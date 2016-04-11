8d0a80335f459e3d014ee6d4d66562c8e1cdf32c
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-11 16:42:57 +0700

    define initViewMainActivity method and initListener method

--- LearnFrameLayout/app/src/main/java/com/example/trungnt/learnframelayout/MainActivity.java
+++ LearnFrameLayout/app/src/main/java/com/example/trungnt/learnframelayout/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.trungnt.learnframelayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnShow, btnHide, btnClose, btnFullScreen, btnMiniature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViewMainActivity();
+        initListener();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    //dinh nghia phuong thuc khoi tao ViewMainActivity
+    private void initViewMainActivity()
+    {
+        btnShow = (Button) this.findViewById(R.id.btnShow);
+        btnHide = (Button) this.findViewById(R.id.btnHide);
+        btnClose = (Button) this.findViewById(R.id.btnClose);
+        btnFullScreen = (Button) this.findViewById(R.id.btnFullscreen);
+        btnMiniature = (Button) this.findViewById(R.id.btnMiniature);
+    }
+
+    private void initListener()
+    {
+        btnShow.setOnClickListener(this);
+        btnHide.setOnClickListener(this);
+        btnClose.setOnClickListener(this);
+        btnFullScreen.setOnClickListener(this);
+        btnMiniature.setOnClickListener(this);
     }
 }
