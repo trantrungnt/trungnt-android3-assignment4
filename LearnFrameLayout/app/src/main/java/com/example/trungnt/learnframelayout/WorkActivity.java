b8030ab6bf79c634a77506d39d18cfce66641b4a
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-11 16:22:31 +0700

    add AuthorActivity class and WorkActivity class

---
+++ LearnFrameLayout/app/src/main/java/com/example/trungnt/learnframelayout/WorkActivity.java
@@ -0,0 +1,15 @@
+package com.example.trungnt.learnframelayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TrungNT on 4/11/2016.
+ */
+public class WorkActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work);
+    }
+}
