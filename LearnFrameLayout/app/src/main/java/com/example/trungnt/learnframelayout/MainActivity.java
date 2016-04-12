8dc1f1a18b744ece8d9c9d2e4ab6f661f67ec27a
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-13 03:47:34 +0700

    fix bug in Mainfest

--- LearnFrameLayout/app/src/main/java/com/example/trungnt/learnframelayout/MainActivity.java
+++ LearnFrameLayout/app/src/main/java/com/example/trungnt/learnframelayout/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
